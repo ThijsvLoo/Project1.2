3eb8542e536d23e4cb0e4c94f893dbff726fc4ca
Smoothgiraffe <32669155+Smoothgiraffe@users.noreply.github.com>
2017-11-07 13:14:45 +0100

    updated descriptions

--- Board.java
+++ Board.java
@@ -10,11 +10,7 @@ public class Board{
 		char[][] board = new[height][width]; //empty board
 	}
 	
-	public char[][] makeBoard(){
-		
-	}
-	
-	public void placingPentomino(int x, int y){
+	public void placingPentomino(int x, int y){ //places Pentomino  in the board
 		while(/*falls*/){
 			
 		}
@@ -25,12 +21,14 @@ public class Board{
 		}
 	}
 	
-	public void fall(char[][] pent){
+	public void fall(char[][] pent){ //moves the current pentomino one space lower 
 		
 	}
 	
-	public void gameOver(){
-		
+	public void gameOver(char[][] pent; char[][] board){ 
+		if (!fits(pent, board, 1, 0) {
+			//Game over code here...
+		}
 	}
 	
 	public void newPentomino(char[][] pent){
@@ -78,18 +76,27 @@ public class Board{
 		}
 	}
 	
-	public void down(char[][] pent){
-		int heightP = pent.length;
-		int widthP = pent[0].length;
-		for(int i = 0; i<heightP; i++){
-			for(int j = 0; j<widthP; j++){
-				if(board[i-1][j] == '0' & i != 0){
-					pent[i][j] = pent[i-1][j];
-				}
-				else{
-					pent[i][j] = pent[i][j];
+	public void down (char[][] pent, char[][] board, int x, int y) {
+	for (int i = y; i > board.length; i++) {
+		if(!fits(pent, board, x, y)) {
+			//place the pentomino in [x][y-1]
+			break;
+			}
+		}
+	}
+		    
+	public boolean fits(char[][] pent, char[][] board, int x, int y) { //checks if the Pentomino fits in the given spot
+		if (x + pent[0].length > board[0].length || y + pent.length > board.length) { //checks for out of bounds
+			return false;
+		} else { //if not out of bound, check if there is something
+			for(int i=0; i < pent.length; i++) {
+				for(int j=0; j < pent[0].length; j++) {
+					if (pent[i][j]!='0' && board[x+i][y+j]!='0') {
+						return false;
+					}
 				}
 			}
+			return true;
 		}
 	}
 }
