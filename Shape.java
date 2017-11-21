c1823b67989e0f446447dc35d151f91685c8cb00
dutchdiddler <32632105+dutchdiddler@users.noreply.github.com>
2017-11-21 13:06:24 +0100

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,4 +1,22 @@
 import java.awt.*
 public class Shape{
-	public shape();
+	// private BufferedImage block;
+	private int[][] coordinates;
+	private Board board;
+	
+	
+	public Shape(BufferedImage block, int[][] coordinates, Board board){
+		this.block = block;
+		this.coordinates = coordinates;
+		this.board = board;
+	}
+	
+	// So we can update shape
+	public void update(){
+		
+	}
+	//So we can render the shape
+	public void render(Graphics g){
+		
+	}
 }
