0c2c545364dbef6c58af6242a8addebae22d634a
Thieskeloo <thijsvanloo4@gmail.com>
2017-11-21 17:28:06 +0100

    made a working game without scoreboard

--- Board.java
+++ Board.java
@@ -1,107 +1,208 @@
-import javax.swing.*;
-import java.awt.*;
-public class Board extends JPanel{
-	//private BufferedImage blocks;
-	private final int blockSize = 50;
-	private final int boardWidth = 5;
-	private final int boardHeight = 15;
-	private int[][] board = new int[boardWidth][boardHeight];
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Board extends JPanel implements KeyListener{
 	
-	// private Shape[] shapes = new Shape[12];
+	private BufferedImage blocks;
+	private final int blockSize = 50;
+	private final int boardWidth = 5, boardHeight = 15;
+	private int[][] board = new int[boardHeight][boardWidth];
+	private Shape[] shapes = new Shape[12];
+	private Shape currentShape;
+	private Timer timer;
+	private final int FPS = 60;
+	private final int delay = 1000/FPS;
+	private boolean gameOver = false;
+	private static int score = 0;
+
 	public Board(){
-		// Where we will insert our "Base" image
-		/* try{
-			blocks = ImageIO.read(Board.class.getResource("/NAME OF IMAGE FILE WE HAVE"));
+		
+		try {
+			blocks = ImageIO.read(Board.class.getResource("/tiles.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		*/
 		
+		timer = new Timer(delay, new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				update();
+				repaint();
+			}
+		});
+		
+		timer.start();
+
 		// Created shapes, but they require the base image to work
-		/*
 		shapes[0] = new Shape(blocks.getSubimage(0, 0, blockSize, blockSize), new int[][] {
-			{1,1,1,1,1} //I shape
-		}, this);
-		
-		shapes[1] = new Shape(blocks.getSubimage(0, 0, blockSize, blockSize), new int[][] {
-			{1,1},
-			{1,1},
-			{1,0} //P shape
-		}, this);
-		
-		shapes[2] = new Shape(blocks.getSubimage(0, 0, blockSize, blockSize), new int[][] {
-			{0,1,0},
-			{1,1,1},
-			{0,1,0} //X shape
-		}, this);
-		
-		shapes[3] = new Shape(blocks.getSubimage(0, 0, blockSize, blockSize), new int[][] {
-			{0,1,1},
-			{1,1,0},
-			{0,1,0} //F shape
-		}, this);
-		
-		shapes[4] = new Shape(blocks.getSubimage(0, 0, blockSize, blockSize), new int[][] {
-			{1,0,0},
-			{1,0,0},
-			{1,1,1}//V shape
-		}, this);
-		
-		shapes[5] = new Shape(blocks.getSubimage(0, 0, blockSize, blockSize), new int[][] {
-			{1,0,0},
-			{1,1,0},
-			{0,1,1} //W shape
-		}, this);
-		
-		shapes[6] = new Shape(blocks.getSubimage(0, 0, blockSize, blockSize), new int[][] {
-			{0,1},
-			{1,1},
-			{0,1},
-			{0,1} //Y shape
-		}, this);
-		
-		shapes[7] = new Shape(blocks.getSubimage(0, 0, blockSize, blockSize), new int[][] {
-			{1,1,1},
-			{0,1,0},
-			{0,1,0} //t shape
-		}, this);
-		
-		shapes[8] = new Shape(blocks.getSubimage(0, 0, blockSize, blockSize), new int[][] {
-			{1,1,0},
-			{0,1,0},
-			{0,1,1} //Z shape
-		}, this);
-		
-		shapes[9] = new Shape(blocks.getSubimage(0, 0, blockSize, blockSize), new int[][] {
-			{1,0,1},
-			{1,1,1} //U shape
-		}, this);
-		
-		shapes[10] = new Shape(blocks.getSubimage(0, 0, blockSize, blockSize), new int[][] {
-			{1,1,0,0},
-			{0,1,1,1} //N shape
-		}, this);
-		
-		shapes[11] = new Shape(blocks.getSubimage(0, 0, blockSize, blockSize), new int[][] {
-			{0,0,0,1},
-			{1,1,1,1} //L shape
-		}, this);
-				*/
-		
+				{1,1,1,1,1} //I shape
+		}, this, 1);
+
+		shapes[1] = new Shape(blocks.getSubimage(blockSize, 0, blockSize, blockSize), new int[][] {
+				{1,1,1},
+				{1,1,0}//P shape
+		}, this, 2);
+
+		shapes[2] = new Shape(blocks.getSubimage(blockSize*2, 0, blockSize, blockSize), new int[][] {
+				{0,1,0},
+				{1,1,1},
+				{0,1,0} //X shape
+		}, this, 3);
+
+		shapes[3] = new Shape(blocks.getSubimage(blockSize*3, 0, blockSize, blockSize), new int[][] {
+				{0,1,1},
+				{1,1,0},
+				{0,1,0} //F shape
+		}, this, 4);
+
+		shapes[4] = new Shape(blocks.getSubimage(blockSize*4, 0, blockSize, blockSize), new int[][] {
+				{1,1,1},
+				{0,0,1},
+				{0,0,1} //V shape
+		}, this, 5);
+
+		shapes[5] = new Shape(blocks.getSubimage(blockSize*5, 0, blockSize, blockSize), new int[][] {
+				{1,1,0},
+				{0,1,1},
+				{0,0,1} //W shape
+		}, this, 6);
+
+		shapes[6] = new Shape(blocks.getSubimage(blockSize*6, 0, blockSize, blockSize), new int[][] {
+				{1,1,1,1},
+				{1,0,0,0} //Y shape
+		}, this, 7);
+
+		shapes[7] = new Shape(blocks.getSubimage(blockSize*7, 0, blockSize, blockSize), new int[][] {
+				{1,1,1},
+				{0,1,0},
+				{0,1,0} //t shape
+		}, this, 8);
+
+		shapes[8] = new Shape(blocks.getSubimage(blockSize*8, 0, blockSize, blockSize), new int[][] {
+				{1,1,0},
+				{0,1,0},
+				{0,1,1} //Z shape
+		}, this, 9);
+
+		shapes[9] = new Shape(blocks.getSubimage(blockSize*9, 0, blockSize, blockSize), new int[][] {
+				{1,0,1},
+				{1,1,1} //U shape
+		}, this, 10);
+
+		shapes[10] = new Shape(blocks.getSubimage(blockSize*10, 0, blockSize, blockSize), new int[][] {
+				{1,1,0,0},
+				{0,1,1,1} //N shape
+		}, this, 11);
+
+		shapes[11] = new Shape(blocks.getSubimage(blockSize*11, 0, blockSize, blockSize), new int[][] {
+				{1,1,1,1},
+				{1,0,0,0}, //L shape
+		}, this, 12);
+
+		setNextShape();
+
+	}
+	
+	public void update(){
+		currentShape.update();
+		if(gameOver)
+			timer.stop();
 	}
 	
 	
 	public void paintComponent(Graphics g){
-		super.paintComponent(g);
-		//drawing horizontal lines
+		super.paintComponent(g);	
+		
+		currentShape.render(g);
+		
+		for(int row = 0; row < board.length; row++)
+			for(int col = 0; col < board[row].length; col++)
+				if(board[row][col] != 0)
+					g.drawImage(blocks.getSubimage((board[row][col]-1)*blockSize, 0, blockSize, blockSize),
+					col*blockSize, row*blockSize, null);
+		
+		
+		
 		for(int i = 0; i < boardHeight; i++){
 			g.drawLine(0, i*blockSize, boardWidth*blockSize, i*blockSize);
 		}
-		//drawing vertical lines
 		for(int j = 0; j < boardWidth; j++){
 			g.drawLine(j*blockSize, 0, j*blockSize, boardHeight*blockSize);
 		}
 		
+	}
+	
+	public void setNextShape(){
+		
+		int index = (int)(Math.random()*shapes.length);
+		
+		Shape newShape = new Shape(shapes[index].getBlock(), shapes[index].getCoords(),
+				this, shapes[index].getColor());
+		
+		currentShape = newShape;
+		
+		for(int row = 0; row < currentShape.getCoords().length; row++)
+			for(int col = 0; col < currentShape.getCoords()[row].length; col++)
+				if(currentShape.getCoords()[row][col] != 0){
+					
+					if(board[row][col] != 0)
+						gameOver = true;
+				}
+		
+		
 		
 	}
+	
+	
+	public int getBlockSize(){
+		return blockSize;
+	}
+
+	public int[][] getBoard(){
+		return board;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void addScore(int score) {
+		this.score += score;
+		System.out.println(this.score);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_LEFT)
+			currentShape.setDeltaX(-1);
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT)
+			currentShape.setDeltaX(1);
+		if(e.getKeyCode() == KeyEvent.VK_DOWN)
+			currentShape.speedDown();
+		if(e.getKeyCode() == KeyEvent.VK_UP)
+			currentShape.rotate();
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_DOWN)
+			currentShape.normalSpeed();
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
--- Window.java
+++ Window.java
@@ -1,25 +1,33 @@
-import javax.swing.*;
-
+import javax.swing.JFrame;
 
 public class Window {
+	
+	public static final int WIDTH = 250, HEIGHT = 780;
 	private JFrame window;
 	private Board board;
-	public static final int WIDTH = 260, HEIGHT = 800;
 	
 	public Window(){
-		window = new JFrame("Pentris Game");
+		window = new JFrame("Tetris Game");
 		window.setSize(WIDTH, HEIGHT);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
 		window.setLocationRelativeTo(null);
-		window.setVisible(true);
 		
 		board = new Board();
+		
+		
+		
 		window.add(board);
+		window.addKeyListener(board);
+		
+		
+		window.setVisible(true);
 	}
 	
-
-	public static void main(String[] args){
-	   new Window();
+	
+	
+	public static void main(String[] args) {
+		new Window();
 	}
+
 }
