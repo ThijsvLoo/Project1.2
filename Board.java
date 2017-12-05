ad60548a9f22300f18cefdfb853d836599dc970f
Smoothgiraffe <32669155+Smoothgiraffe@users.noreply.github.com>
2017-12-05 14:30:10 +0100

    Created Board.java for the bot

---
+++ Board.java
@@ -0,0 +1,219 @@
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.*;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import static java.lang.System.*;
+import java.awt.event.ActionListener; //is needed for the timer!
+import java.awt.event.ActionEvent;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Board extends JPanel {
+
+	private BufferedImage blocks;
+	private final int blockSize = 50;
+	private final int boardWidth = 5, boardHeight = 15;
+	private int[][] board = new int[boardHeight][boardWidth];
+	private Shape[] shapes = new Shape[12];
+	private Shape currentShape;
+	private Timer timer;
+	private final int FPS = 60;
+	private final int delay = 1000/FPS;
+	private boolean gameOver = false;
+	private static int score = 0;
+
+	public Board(){
+
+		try {
+			blocks = ImageIO.read(Board.class.getResource("/tiles.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		timer = new Timer(delay, new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				update();
+				repaint();
+			}
+		});
+
+		timer.start();
+
+		// Created shapes, but they require the base image to work
+		shapes[0] = new Shape(blocks.getSubimage(0, 0, blockSize, blockSize), new int[][] {
+				{1,1,1,1,1} //I shape
+		}, this, 1);
+
+		shapes[1] = new Shape(blocks.getSubimage(blockSize, 0, blockSize, blockSize), new int[][] {
+				{1,1,1},
+				{1,1,0}//P shape
+		}, this, 2);
+
+		shapes[2] = new Shape(blocks.getSubimage(blockSize*2, 0, blockSize, blockSize), new int[][] {
+				{0,1,0},
+				{1,1,1},
+				{0,1,0} //X shape
+		}, this, 3);
+
+		shapes[3] = new Shape(blocks.getSubimage(blockSize*3, 0, blockSize, blockSize), new int[][] {
+				{0,1,1},
+				{1,1,0},
+				{0,1,0} //F shape
+		}, this, 4);
+
+		shapes[4] = new Shape(blocks.getSubimage(blockSize*4, 0, blockSize, blockSize), new int[][] {
+				{1,1,1},
+				{0,0,1},
+				{0,0,1} //V shape
+		}, this, 5);
+
+		shapes[5] = new Shape(blocks.getSubimage(blockSize*5, 0, blockSize, blockSize), new int[][] {
+				{1,1,0},
+				{0,1,1},
+				{0,0,1} //W shape
+		}, this, 6);
+
+		shapes[6] = new Shape(blocks.getSubimage(blockSize*6, 0, blockSize, blockSize), new int[][] {
+				{1,1,1,1},
+				{1,0,0,0} //Y shape
+		}, this, 7);
+
+		shapes[7] = new Shape(blocks.getSubimage(blockSize*7, 0, blockSize, blockSize), new int[][] {
+				{1,1,1},
+				{0,1,0},
+				{0,1,0} //t shape
+		}, this, 8);
+
+		shapes[8] = new Shape(blocks.getSubimage(blockSize*8, 0, blockSize, blockSize), new int[][] {
+				{1,1,0},
+				{0,1,0},
+				{0,1,1} //Z shape
+		}, this, 9);
+
+		shapes[9] = new Shape(blocks.getSubimage(blockSize*9, 0, blockSize, blockSize), new int[][] {
+				{1,0,1},
+				{1,1,1} //U shape
+		}, this, 10);
+
+		shapes[10] = new Shape(blocks.getSubimage(blockSize*10, 0, blockSize, blockSize), new int[][] {
+				{1,1,0,0},
+				{0,1,1,1} //N shape
+		}, this, 11);
+
+		shapes[11] = new Shape(blocks.getSubimage(blockSize*11, 0, blockSize, blockSize), new int[][] {
+				{1,1,1,1},
+				{1,0,0,0}, //L shape
+		}, this, 12);
+
+		setNextShape();
+
+	}
+
+	public void update(){
+		currentShape.update();
+		if(gameOver)
+			timer.stop();
+	}
+
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+
+		currentShape.render(g);
+
+		for(int row = 0; row < board.length; row++)
+			for(int col = 0; col < board[row].length; col++)
+				if(board[row][col] != 0)
+					g.drawImage(blocks.getSubimage((board[row][col]-1)*blockSize, 0, blockSize, blockSize),
+					col*blockSize, row*blockSize, null);
+
+		for(int i = 0; i < boardHeight; i++){
+			g.drawLine(0, i*blockSize, boardWidth*blockSize, i*blockSize);
+		}
+		for(int j = 0; j < boardWidth; j++){
+			g.drawLine(j*blockSize, 0, j*blockSize, boardHeight*blockSize);
+		}
+	}
+
+	public void setNextShape(){
+
+		int index = (int)(Math.random()*shapes.length);
+
+		Shape newShape = new Shape(shapes[index].getBlock(), shapes[index].getCoords(),
+				this, shapes[index].getColor());
+
+		currentShape = newShape;
+
+		for(int row = 0; row < currentShape.getCoords().length; row++)
+			for(int col = 0; col < currentShape.getCoords()[row].length; col++)
+				if(currentShape.getCoords()[row][col] != 0){
+
+					if(board[row][col] != 0)
+						gameOver = true;
+				}
+
+				bot();
+
+	}
+
+
+	public int getBlockSize(){
+		return blockSize;
+	}
+
+	public int[][] getBoard(){
+		return board;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void addScore(int amount) {
+		score += amount;
+		System.out.println(score);
+	}
+
+	public int getBoardWidth() {
+		return boardWidth;
+	}
+
+	public int getBoardHeight() {
+		return boardHeight;
+	}
+
+	public void shiftLeft() {
+		currentShape.setDeltaX(-1);
+	}
+
+	public void shiftRight() {
+		currentShape.setDeltaX(1);
+	}
+
+	public void goFaster() {
+		currentShape.speedDown();
+	}
+
+	public void rotate() {
+		currentShape.rotate();
+	}
+
+	public boolean getGameOver() {
+		return gameOver;
+	}
+
+	public void bot() {
+		while(something) {
+			int[][] board2 = new int[boardHeight][boardWidth];
+			for (int i = 0; i<boardHeight; i++) {
+				for (int j = 0; j<boardWidth; j++) {
+					board2[i][j] = board[i][j];
+				}
+			}
+		}
+	}
+}
