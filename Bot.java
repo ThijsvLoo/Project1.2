7547ef69ff2742e38eb70dedb735eedff1f09730
Smoothgiraffe <32669155+Smoothgiraffe@users.noreply.github.com>
2017-12-12 12:24:46 +0100

    Add files via upload

--- Bot.java
+++ Bot.java
@@ -3,19 +3,31 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.Random;
 
+
+/**
+* This is a very simple bot that randomly decides to rotate the pentomino, shifts it left or right or does nothing.
+*/
+
 public class Bot {
 
   public Bot() {
     Random rand = new Random();
     try {
-      Robot robot = new Robot();
-      int x = rand.nextInt(85);
 
-      if(x == 0) {
+      /**
+      * The Robot object is able to emulate the keyPresses that the user would normally input.
+      */
+      Robot robot = new Robot();
+      /**
+      * The keySetter, a random number that was chosen will determine what the program does.
+      */
+      int keySetter = rand.nextInt(85);
+
+      if(keySetter == 0) {
         robot.keyPress(KeyEvent.VK_UP);
-      } else if (x == 1) {
+      } else if (keySetter == 1) {
         robot.keyPress(KeyEvent.VK_LEFT);
-      } else if (x == 2) {
+      } else if (keySetter == 2) {
         robot.keyPress(KeyEvent.VK_RIGHT);
       }
 
